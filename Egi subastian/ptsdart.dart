44a5274b1e3da153fc390aad9c974b9ca5f42319
Egisubastian <110141363+Egisubastian@users.noreply.github.com>
2023-05-12 00:38:23 +0700

    Add files via upload

---
+++ Egi subastian/ptsdart.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+// void main() {
+//   stdout.write("Masukkan angka: ");
+//   var input = int.parse(stdin.readLineSync()!);
+  
+//   if (input > 0) {
+//     print("Angka yang diinputkan positif");
+//   } else if (input == 0) {
+//     print("Angka yang diinputkan nol");
+//   } else {
+//     print("Angka yang diinputkan negatif");
+//   }
+
+// }
+
+
+
+//  void main() {
+//    for (int i = 1; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//        print(i);
+//      }
+//    }
+//  }
+
+
+// void main() {
+//   for (int i = 4; i <= 50; i += 4) {
+//     print(i);
+//   }
+// }
+
+// void main() {
+//   List<String> merkLaptop = ["Apple", "Dell", "Lenovo", "Asus", "HP"];
+//   print(merkLaptop);
+// }
+
+//  void main() {
+//    var now = DateTime.now();
+
+//   if (now.weekday >= 7 && now.weekday <= 7) {
+//     print("Anda harus bekerja");
+//   } else {
+//     print("Hari ini adalah hari libur");
+//   }
+// }
